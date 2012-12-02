1c86138adc014ad05b64c560474f1d171f49272e
Kyle Montag <montag.k@husky.neu.edu>
2012-12-02 17:42:44 -0500

    lots of s tuff

--- history/historyofcg/forms.py
+++ history/historyofcg/forms.py
@@ -6,7 +6,8 @@ class PageForm(ModelForm):
     class Meta:
         model = Page
         widgets = {
-            'tags': forms.SelectMultiple()
+            'tags': forms.SelectMultiple(attrs={'class': 'entry-tags fields'}),
+            'type': forms.Select(attrs={'class': 'entry-type-select none', 'id': 'EntryCategoryId'})
         }
 
 __author__ = 'Kyle'
