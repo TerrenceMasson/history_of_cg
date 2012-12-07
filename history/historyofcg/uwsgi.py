c6a73a415685c35904211d84fa4a56dd8524fbdb
Kyle Montag <montag.k@husky.neu.edu>
2012-12-07 13:01:56 -0500

    working on webserver

---
+++ history/historyofcg/uwsgi.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import site
+
+site.addsitedir('/opt/apps/env/lib/python2.6/site-packages')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'historyofcg.settings'
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
+socket = '0.0.0.0:49152'
