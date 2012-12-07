315b1b51613211d232d23e355961714b50d3cfa6
Kyle Montag <montag.k@husky.neu.edu>
2012-12-07 09:30:16 -0500

    removed env from the repo, added requirements.txt :D:D

---
+++ history/fabfile.py
@@ -0,0 +1,30 @@
+""" Deployment of your django project.
+"""
+
+from fabric.api import *
+
+env.hosts = ['historyofcg.com']
+env.user = "root"
+
+def update_django_project():
+    """ Updates the remote django project.
+    """
+    with cd(settings.PROJECT_PATH):
+        run('git pull')
+        with prefix('source /historyofcg/dj_history/env/Scripts/activate'):
+            run('pip install -r requirements.txt')
+            run('python manage.py syncdb')
+            run('python manage.py migrate') # if you use south
+            run('python manage.py collectstatic --noinput')
+
+def restart_webserver():
+    """ Restarts remote nginx and uwsgi.
+    """
+    sudo("service uwsgi restart")
+    sudo("/etc/init.d/nginx restart")
+
+def deploy():
+    """ Deploy Django Project.
+    """
+    update_django_project()
+    restart_webserver()
