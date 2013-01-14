fab721f1f780f30f3cc3e59bb65c216a5be4af69
Kyle Montag <montag.k@husky.neu.edu>
2013-01-14 15:58:06 -0500

    login flow fixes and styling. fixing stlying on grid.

--- history/historyofcg/templatetags/hocgtags.py
+++ history/historyofcg/templatetags/hocgtags.py
@@ -1,4 +1,8 @@
+import StringIO
+import struct
+import urllib2
 from django import template
+from django.core.files.images import ImageFile
 from django.template import resolve_variable, NodeList
 from django.contrib.auth.models import Group
 from historyofcg.models import Review
@@ -80,4 +84,72 @@ class GroupCheckNode(template.Node):
 
         return self.nodelist_false.render(context)
 
+
+@register.filter
+def get_img_width(value):
+    header = urllib2.urlopen(value).read(24)
+    return_list = getImageInfo(header)
+
+    print 'c', return_list[0], 'w', return_list[1], 'h', return_list[2]
+    return int(return_list[1])
+
+def getImageInfo(data):
+    data = str(data)
+    size = len(data)
+    height = -1
+    width = -1
+    content_type = ''
+
+    # handle GIFs
+    if (size >= 10) and data[:6] in ('GIF87a', 'GIF89a'):
+        # Check to see if content_type is correct
+        content_type = 'image/gif'
+        w, h = struct.unpack("<HH", data[6:10])
+        width = int(w)
+        height = int(h)
+
+    # See PNG 2. Edition spec (http://www.w3.org/TR/PNG/)
+    # Bytes 0-7 are below, 4-byte chunk length, then 'IHDR'
+    # and finally the 4-byte width, height
+    elif ((size >= 24) and data.startswith('\211PNG\r\n\032\n')
+          and (data[12:16] == 'IHDR')):
+        content_type = 'image/png'
+        w, h = struct.unpack(">LL", data[16:24])
+        width = int(w)
+        height = int(h)
+
+    # Maybe this is for an older PNG version.
+    elif (size >= 16) and data.startswith('\211PNG\r\n\032\n'):
+        # Check to see if we have the right content type
+        content_type = 'image/png'
+        w, h = struct.unpack(">LL", data[8:16])
+        width = int(w)
+        height = int(h)
+
+    # handle JPEGs
+    elif (size >= 20):
+    # and data.startswith('\377\370'):
+        content_type = 'image/jpeg'
+        jpeg = StringIO.StringIO(data)
+        jpeg.read(2)
+        b = jpeg.read(1)
+        try:
+            while (b and ord(b) != 0xDA):
+                while (ord(b) != 0xFF): b = jpeg.read(1)
+                while (ord(b) == 0xFF): b = jpeg.read(1)
+                if (ord(b) >= 0xC0 and ord(b) <= 0xC3):
+                    jpeg.read(3)
+                    h, w = struct.unpack(">HH", jpeg.read(4))
+                    break
+                else:
+                    jpeg.read(int(struct.unpack(">H", jpeg.read(2))[0])-2)
+                b = jpeg.read(1)
+            width = int(w)
+            height = int(h)
+        except struct.error:
+            pass
+        except ValueError:
+            pass
+
+    return content_type, width, height
 __author__ = 'Kyle'
