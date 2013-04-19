2458e22cc0775013210ad7cbc07a0b75ec2e2584
Kyle Montag <montag.k@husky.neu.edu>
2013-04-19 10:53:50 -0400

    bug 106: story buttons.
    need to figure out a smooth way to great them out.

--- history/historyofcg/static/js/common.js
+++ history/historyofcg/static/js/common.js
@@ -16,50 +16,70 @@ $(function () {
     $('.opener').click(function () {
         var story = $(this)[0];
         var container = $(this).parent('.tile');
+        var disable_lt = false;
+        var disable_gt = false;
 
         console.log(container);
 
         function reset_buttons() {
-            var buttons = {};
-            if (container[0].previousElementSibling && container[0].previousElementSibling.getAttribute('class').indexOf('dynamic') == -1) {
-                buttons["<"] = function () {
-                    console.log('previous button');
-                    container = container.prev();
-                    if (container.hasClass('story-image')) {
-                        $dialog.html('<img style="width:590px;" src=' + container[0].getAttribute('data-image') + '>');
+            var buttons = [];
+            buttons[0] = {
+                id: "button_lt",
+                text: "<",
+                click: function () {
+                    if (container[0].previousElementSibling && container[0].previousElementSibling.getAttribute('class').indexOf('dynamic') == -1) {
+                        console.log('previous button');
+                        container = container.prev();
+                        if (container.hasClass('story-image')) {
+                            $dialog.html('<img style="width:590px;" src=' + container[0].getAttribute('data-image') + '>');
+                        }
+                        if (container.hasClass('story-video')) {
+                            $dialog.html('<div class="container"><embed src="http://www.youtube.com/v/' + container[0].getAttribute('data-video') + '" type="application/x-shockwave-flash" width="590px" height="443" allowscriptaccess="always"></div>');
+                        }
+                        if (container.hasClass('story-text')) {
+                            $dialog.html('<div class="container"><span>' + container[0].getAttribute('data-full-text') + '</span></div>');
+                        }
+                        $dialog.dialog("option", "title", container.innerText);
+                        $dialog.dialog("option", "position", {my: "center", at: "center", of: window});
+                        $dialog.dialog('option', 'buttons', reset_buttons());
+                    } else {
+                        console.log("lt_disable");
+                        disable_lt = true;
                     }
-                    if (container.hasClass('story-video')) {
-                        $dialog.html('<div class="container"><embed src="http://www.youtube.com/v/' + container[0].getAttribute('data-video') + '" type="application/x-shockwave-flash" width="590px" height="443" allowscriptaccess="always"></div>');
-                    }
-                    if (container.hasClass('story-text')) {
-                        $dialog.html('<div class="container"><span>' + container[0].getAttribute('data-full-text') + '</span></div>');
-                    }
-                    $dialog.dialog("option", "title", container.innerText);
-                    $dialog.dialog("option", "position", {my: "center", at: "center", of: window});
-                    $dialog.dialog('option', 'buttons', reset_buttons());
-                };
-            }
-            buttons["X"] = function () {
-                $dialog.dialog("close");
+                }
             };
-            if (container[0].nextElementSibling && container[0].nextElementSibling.getAttribute('class').indexOf('connection') == -1) {
-                buttons[">"] = function () {
-                    console.log('next button');
-                    container = container.next();
-                    if (container.hasClass('story-image')) {
-                        $dialog.html('<img style="width:590px;" src=' + container[0].getAttribute('data-image') + '>');
+            buttons[1] = {
+                id: "button_x",
+                text: "X",
+                click: function () {
+                    $dialog.dialog("close");
+                }
+            };
+            buttons[2] = {
+                id: "button_gt",
+                text: ">",
+                click: function () {
+                    if (container[0].nextElementSibling && container[0].nextElementSibling.getAttribute('class').indexOf('connection') == -1) {
+                        console.log('next button');
+                        container = container.next();
+                        if (container.hasClass('story-image')) {
+                            $dialog.html('<img style="width:590px;" src=' + container[0].getAttribute('data-image') + '>');
+                        }
+                        if (container.hasClass('story-video')) {
+                            $dialog.html('<div class="container"><embed src="http://www.youtube.com/v/' + container[0].getAttribute('data-video') + '" type="application/x-shockwave-flash" width="590px" height="443" allowscriptaccess="always"></div>');
+                        }
+                        if (container.hasClass('story-text')) {
+                            $dialog.html('<div class="container"><span>' + container[0].getAttribute('data-full-text') + '</span></div>');
+                        }
+                        $dialog.dialog("option", "title", 'Story story_id of story_total');
+                        $dialog.dialog("option", "position", {my: "center", at: "center", of: window});
+                        $dialog.dialog('option', 'buttons', reset_buttons());
+                    } else {
+                        console.log("gt_disable");
+                        disable_gt = true;
                     }
-                    if (container.hasClass('story-video')) {
-                        $dialog.html('<div class="container"><embed src="http://www.youtube.com/v/' + container[0].getAttribute('data-video') + '" type="application/x-shockwave-flash" width="590px" height="443" allowscriptaccess="always"></div>');
-                    }
-                    if (container.hasClass('story-text')) {
-                        $dialog.html('<div class="container"><span>' + container[0].getAttribute('data-full-text') + '</span></div>');
-                    }
-                    $dialog.dialog("option", "title", 'Story story_id of story_total');
-                    $dialog.dialog("option", "position", {my: "center", at: "center", of: window});
-                    $dialog.dialog('option', 'buttons', reset_buttons());
-                };
-            }
+                }
+            };
 
             return buttons;
         }
@@ -80,6 +100,13 @@ $(function () {
         console.log('buttons drawn');
         $dialog.dialog('open');
         // prevent the default action, e.g., following a link
+        if (disable_lt) {
+            $("button_lt").hide();
+        }
+
+        if (disable_gt) {
+            $("button_gt").hide();
+        }
         return false;
     });
 
