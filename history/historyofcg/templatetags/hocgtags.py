deb886fdb98b21f623fd2a37880e402880205d6a
Kyle Montag <montag.k@husky.neu.edu>
2013-04-19 09:50:09 -0400

    edwin catmull bug.

--- history/historyofcg/templatetags/hocgtags.py
+++ history/historyofcg/templatetags/hocgtags.py
@@ -30,7 +30,7 @@ def replace(value, args):
 
 @register.filter
 def get_random_image(value):
-    images = Story.objects.filter(page__vanity_url=value.vanity_url, published=True, image__isnull=False)
+    images = Story.objects.filter(page__vanity_url=value.vanity_url, published=True, text__isnull=True, video__isnull=True)
     page = Page.objects.get(vanity_url = value.vanity_url)
     if len(images) == 0:
         return "/static/img/P_desatCentered02.png" if page.type.name != "person" else "http://www.clker.com/cliparts/q/T/l/N/J/S/blank-profile-md.png"
