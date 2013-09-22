8042965589231c3f3f79995ac56cb17bb52953b8
Gowiem <MGowie@apprenaissance.com>
2013-09-22 18:10:34 -0400

    Adds changes to css files which were staged somehow. Still fuck everything that just happened.

--- history/logger.py
+++ history/logger.py
@@ -1,12 +1,12 @@
 import inspect, logging, os
 from django.conf import settings
 
-def log(message):
+def log(*message):
+    "Automatically log the current function details."
     if not settings.DEBUG:
         return
-    "Automatically log the current function details."
-    # Get the previous frame in the stack, otherwise it would
-    # be this function!!!
+    message = "%s" % list(message)
+    # Get the previous frame in the stack, otherwise it would be this function
     func = inspect.currentframe().f_back.f_code
     # Dump the message + the name of this function to the log.
     logging.debug("%s:%i - method: %s ===============\n=> %s \n ===============" % (
