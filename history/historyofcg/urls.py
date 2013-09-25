fee699a64ce7815b8e0b7f3db3b0fb2b5e1401ff
Gowiem <MGowie@apprenaissance.com>
2013-09-25 13:23:28 -0400

    Adds favicon to base.html

--- history/historyofcg/urls.py
+++ history/historyofcg/urls.py
@@ -1,16 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'base.views.home', name='home'),
-    # url(r'^base/', include('base.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
