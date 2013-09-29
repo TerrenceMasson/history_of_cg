43e59b45a84bd80174c1f094f2f0e3899230af86
Gowiem <MGowie@apprenaissance.com>
2013-09-29 00:20:43 -0400

    Updates staging settings for Heroku Postgres DB

--- history/settings/production.py
+++ history/settings/production.py
@@ -8,11 +8,11 @@ print >> sys.stderr, "*** RUNNING PRODUCTION SETTINGS ***"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'd9ed7eiaff0eij',                      # Or path to database file if using sqlite3.
-        'USER': 'olwqqhvokshkbc',                      # Not used with sqlite3.
-        'PASSWORD': '3HX2T9zzb0dwP3S36Do_mQIFFl',                  # Not used with sqlite3.
-        'HOST': 'ec2-54-235-134-222.compute-1.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd9ed7eiaff0eij',
+        'USER': 'olwqqhvokshkbc',
+        'PASSWORD': '3HX2T9zzb0dwP3S36Do_mQIFFl',
+        'HOST': 'ec2-54-235-134-222.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
