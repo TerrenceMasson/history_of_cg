ff749867b1c2bb9a877110d025e2da8d39beb7b0
Gowiem <gowie.matt@gmail.com>
2013-11-18 12:04:04 -0500

    Fixes no Hist.Timeline in common.js

--- history/historyofcg/static/js/common.js
+++ history/historyofcg/static/js/common.js
@@ -2,7 +2,7 @@ var Hist = Hist || {};
 
 $(document).ready(function() {
     Hist.Notifications.init();
-    Hist.Timeline.init();
+    if (Hist.Timeline) { Hist.Timeline.init(); }
 });
 
 // jQuery Additions
