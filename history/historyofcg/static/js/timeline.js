9e65ce407418446f2ee4befcb89424c6fd83431b
Gowiem <gowie.matt@gmail.com>
2013-12-04 17:33:06 -0500

    Plots points according to their year count

--- history/historyofcg/static/js/timeline.js
+++ history/historyofcg/static/js/timeline.js
@@ -1,12 +1,12 @@
 var Hist = Hist || {};
 
 Hist.Timeline = (function() {
-  var momentDates = [],
-      xAxisValues = [],
-      margin = {top: 40, right: 40, bottom: 40, left:40},
+  var timelinePoints = [],
+      margin = {top: 40, right: 40, bottom: 40, left: 40},
       width = 900,
       height = 300,
-      chart;
+      yPositions = {},
+      xScale, beginning, ending,  chart;
 
   var initD3Chart = function() {
     chart = d3.select('.timeline')
@@ -15,17 +15,15 @@ Hist.Timeline = (function() {
             .append("g")
               .attr("transform", "translate(30,0)");
 
-    var jsDates   = momentDates.map(function(m) { return m.toDate(); }),
-        beginning = roundToDecade(d3.min(jsDates), true),
-        ending    = roundToDecade(d3.max(jsDates));
+    var jsDates = timelinePoints.map(function(p) { return p.date.toDate(); });
+    beginning   = roundToDecade(d3.min(jsDates), true),
+    ending      = roundToDecade(d3.max(jsDates));
+    yPositions  = findYPositions();
 
-    var scaleFactor = (1/ (ending - beginning)) * ( width - margin.left - margin.right );
-    console.log("ending: ", ending, " beginning: ", beginning, " Scale Factor: ", scaleFactor);
-
-    var xScale = d3.time.scale()
-              .nice(d3.time.year, 100)
-              .domain([beginning, ending])
-              .range([0, width - margin.right - margin.left])
+    xScale = d3.time.scale()
+                    .nice(d3.time.year, 100)
+                    .domain([beginning, ending])
+                    .range([0, width - margin.right - margin.left]);
               
     var xAxis = d3.svg.axis()
                       .scale(xScale)
@@ -36,27 +34,53 @@ Hist.Timeline = (function() {
       .attr('transform', 'translate(0, ' + (height - margin.top - margin.bottom) + ')')
       .call(xAxis);
 
-    chart.selectAll(".event")
-      .data(jsDates)
+    chart.selectAll(".timeline-item")
+      .data(timelinePoints)
     .enter().append("circle")
-      .attr("class", "shred")
-      .attr("x", xScale)
-      .attr("y", 10)
-      .attr("cx", xScale)
-      .attr("cy", 10)
+      .attr("class", "timeline-item")
+      .attr("x", getXPosition)
+      .attr("y", getYPosition)
+      .attr("cx", getXPosition)
+      .attr("cy", getYPosition)
       .attr("r", 10)
       .attr("height", 10)
       .attr("width", 10)
-      .style("fill", d3.scale.category20(10))
-      .text(function(m) {return m.getYear(); });
+      .style("fill", d3.scale.category20c())
+      .text(function(p) { return p.date.year(); });
+  }
+
+  var getXPosition = function(point) {
+    return xScale(point.date.toDate());
+  }
+
+  var getYPosition = function(point) {
+    return margin.top + 50 * yPositions[point.id];
+  }
+
+  // Loop through the timelinePoints and count those which have the same year.
+  // We use this later to determine if we should stack a point. 
+  var findYPositions = function() {
+    // I think I might just be braindead at this point. This can't be the
+    // easiest way to do this..
+    timelinePoints.forEach(function(point, idx) {
+      var count = 0;
+      timelinePoints.forEach(function(p, idx) {
+        if (point.date.year() === p.date.year() && point.id !== p.id && !p.counted) {
+          count += 1;
+        }
+      });
+      yPositions[point.id] = count;
+      point.counted = true;
+    });
+    return yPositions;
   }
 
   var roundToDecade = function(date, shouldFloor) {
     var year = date.getFullYear(),
         remainder = year % 10,
-        roundedYear = shouldFloor ? (year - remainder) - 10 : (year - remainder) + 10;
-    date.setFullYear(roundedYear);
-    return date;
+        roundedYear = shouldFloor ? (year - remainder) - 10 : (year - remainder) + 10,
+        roundedDate = new Date(date.getTime()).setFullYear(roundedYear);
+    return roundedDate;
   }
 
   return {
@@ -64,10 +88,13 @@ Hist.Timeline = (function() {
       Hist.rawPages.map(function(page) {
         if (page['fields']['date_established']) {
           var mDate = moment(page['fields']['date_established']);
-          momentDates.push(mDate);
+          timelinePoints.push({ 'date':mDate, 'id':page['pk'], counted: false});
         }
       });
-      momentDates.sort();
+
+      timelinePoints.map(function(p, idx) {
+        console.log("Point: ", p, " year: ", p.date.year());
+      });
       initD3Chart();
     }
   }
