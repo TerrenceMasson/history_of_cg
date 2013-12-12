a4255c4d7f5a7ea1dbb69391feccbb2dc5cf785f
Gowiem <gowie.matt@gmail.com>
2013-12-12 17:06:53 -0500

    Adds Array#clone method

--- history/historyofcg/static/js/proto-extensions.js
+++ history/historyofcg/static/js/proto-extensions.js
@@ -1,16 +1,23 @@
 // String Additions
 ////////////////////
 String.prototype.contains = function(substring, ignoreCase) {
-    var lowerSubString = substring.toLowerCase(),
-        lowerThis = this.toLowerCase();
-    if (ignoreCase) {
-        return lowerThis.indexOf(lowerSubString) > -1;
-    } else {
-        return this.indexOf(substring) > -1;
-    }
+  var lowerSubString = substring.toLowerCase(),
+  lowerThis = this.toLowerCase();
+  if (ignoreCase) {
+    return lowerThis.indexOf(lowerSubString) > -1;
+  } else {
+    return this.indexOf(substring) > -1;
+  }
 }
 
 
 String.prototype.isEmpty = function() {
-    return this.trim() === "";
+  return this.trim() === "";
+}
+
+// Array Additions
+///////////////////
+
+Array.prototype.clone = function() {
+  return this.slice(0);
 }
