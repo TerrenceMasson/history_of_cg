8d67b4be199152eb0c26fd781336f0287cf36485
Gowiem <gowie.matt@gmail.com>
2014-04-13 21:32:27 -0400

    Fix S3 url encoding once and for all (I hope)

--- history/historyofcg/views.py
+++ history/historyofcg/views.py
@@ -276,6 +276,9 @@ def sign_s3_upload(request):
     put_request = "PUT\n\n%s\n%d\n%s\n/%s/%s" % (mime_type, expires, amz_headers, S3_BUCKET, new_file_name)
 
     signature = base64.encodestring(hmac.new(AWS_SECRET_KEY, put_request, sha1).digest())
+
+    # Gotta #quote_plus it twice: http://stackoverflow.com/a/20712482/1159410
+    signature = urllib.quote_plus(signature.strip())
     signature = urllib.quote_plus(signature.strip())
 
     url = 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, new_file_name)
