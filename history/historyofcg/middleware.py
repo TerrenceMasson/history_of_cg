15349f6e27a894992403a77e0b4b8f11b0d135c6
Matt Gowie <gowie.matt@gmail.com>
2017-03-30 15:02:05 +0700

    I don't know how to Python

--- history/historyofcg/middleware.py
+++ history/historyofcg/middleware.py
@@ -4,13 +4,12 @@ from django.http import HttpResponseRedirect
 class HttpsRedirect(object):
     # Redirect http => https
     def process_request(self, request):
-        print >> sys.stderr, request.scheme
-        if request.scheme != 'https':
-            # print >> sys.stderr, "TRYING TO REDIRECT"
-            # request_url = request.build_absolute_uri(request.get_full_path())
-            # secure_url = request_url.replace('http://', 'https://')
-            # print >> sys.stderr, "RETURNING REDIRECT WITH URL " + secure_url
-            secure_url = "https://google.com"
+        print >> sys.stderr, str(request.is_secure())
+        if !request.is_secure():
+            print >> sys.stderr, "TRYING TO REDIRECT"
+            request_url = request.build_absolute_uri(request.get_full_path())
+            secure_url = request_url.replace('http://', 'https://')
+            print >> sys.stderr, "RETURNING REDIRECT WITH URL " + secure_url
             return HttpResponseRedirect(secure_url)
         print >> sys.stderr, "RETURNING NONE"
         return None
