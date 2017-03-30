9d36c077b1dd0979646671bdc2024504c9e898e7
Matt Gowie <gowie.matt@gmail.com>
2017-03-30 15:04:43 +0700

    Alright, now this is getting rediculous

--- history/historyofcg/middleware.py
+++ history/historyofcg/middleware.py
@@ -5,7 +5,7 @@ class HttpsRedirect(object):
     # Redirect http => https
     def process_request(self, request):
         print >> sys.stderr, str(request.is_secure())
-        if not !request.is_secure():
+        if not request.is_secure():
             print >> sys.stderr, "TRYING TO REDIRECT"
             request_url = request.build_absolute_uri(request.get_full_path())
             secure_url = request_url.replace('http://', 'https://')
