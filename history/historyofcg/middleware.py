4c34b011314af616a7fd39729459ef4de2f6bbe5
Matt Gowie <gowie.matt@gmail.com>
2017-03-30 14:56:02 +0700

    Slowly but surely

--- history/historyofcg/middleware.py
+++ history/historyofcg/middleware.py
@@ -4,14 +4,12 @@ from django.http import HttpResponseRedirect
 class HttpsRedirect(object):
     # Redirect http => https
     def process_request(self, request):
+        print >> sys.stderr, "SCHEME: " + request.scheme
+        if request.scheme != 'https':
+            print >> sys.stderr, "TRYING TO REDIRECT"
+            request_url = request.build_absolute_uri(request.get_full_path())
+            secure_url = request_url.replace('http://', 'https://')
+            print >> sys.stderr, "RETURNING REDIRECT WITH URL " + secure_url
+            return HttpResponseRedirect(secure_url)
+        print >> sys.stderr, "RETURNING NONE"
         return None
-        # print >> sys.stderr, "SCHEME: " + request.scheme
-        # if request.scheme != 'https':
-        #     print >> sys.stderr, "TRYING TO REDIRECT"
-        #     request_url = request.build_absolute_uri(request.get_full_path())
-        #     secure_url = request_url.replace('http://', 'https://')
-        #     print >> sys.stderr, "RETURNING REDIRECT WITH URL " + secure_url
-        #     return HttpResponseRedirect(secure_url)
-        # else:
-        #     print >> sys.stderr, "RETURNING NONE"
-        #     return None
