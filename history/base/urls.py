b57d9bd6a030de05619fafaea77fe60ad376f2bd
Tung Huynh <tung75605@gmail.com>
2020-05-25 07:07:13 +0000

    urls no longer use patterns()

--- history/base/urls.py
+++ history/base/urls.py
@@ -1,13 +1,15 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+from historyofcg import views
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'base.views.home', name='home'),
     # url(r'^base/', include('base.foo.urls')),
@@ -17,32 +19,32 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'historyofcg.views.home'),
-    url(r'^about/$', 'historyofcg.views.about'),
+    url(r'^$', views.home),
+    url(r'^about/$', views.about),
     url(r'^accounts/', include('registration.backends.default.urls')),
 
-    url(r'^pages/(?P<s>[-\w]+)/$', 'historyofcg.views.view_source_entries'),
-    url(r'^add/page/$', 'historyofcg.views.add_page', name="add_page"),
-    url(r'^edit/page/(?P<vanity_url>[-\w]+)/$', 'historyofcg.views.edit_page'),
-    url(r'^unpublish/page/(?P<vanity_url>[-\w]+)/$', 'historyofcg.views.unpublish_page'),
-    url(r'^publish/page/(?P<vanity_url>[-\w]+)/$', 'historyofcg.views.publish_page'),
+    url(r'^pages/(?P<s>[-\w]+)/$', views.view_source_entries),
+    url(r'^add/page/$', views.add_page, name="add_page"),
+    url(r'^edit/page/(?P<vanity_url>[-\w]+)/$', views.edit_page),
+    url(r'^unpublish/page/(?P<vanity_url>[-\w]+)/$', views.unpublish_page),
+    url(r'^publish/page/(?P<vanity_url>[-\w]+)/$', views.publish_page),
 
-    url(r'^edit/story/(?P<type>[-\w]+)/(?P<id>\d+)/$', 'historyofcg.views.edit_story'),
-    url(r'^publish/story/(?P<id>\d+)/$', 'historyofcg.views.publish_story'),
-    url(r'^unpublish/story/(?P<id>\d+)/$', 'historyofcg.views.unpublish_story'),
-    url(r'^save/story/(?P<story_type>[-\w]+)/(?P<vanity_url>[-\w]+)/$', 'historyofcg.views.new_story'),
-    url(r'^delete/story/(?P<id>\d+)/$', 'historyofcg.views.delete_story'),
-    url(r'^vote/up/(?P<story_id>\d+)/$', 'historyofcg.views.up_vote_story'),
-    url(r'^vote/down/(?P<story_id>\d+)/$', 'historyofcg.views.down_vote_story'),
-    url(r'^vote/none/(?P<story_id>\d+)/$', 'historyofcg.views.no_vote_story'),
+    url(r'^edit/story/(?P<type>[-\w]+)/(?P<id>\d+)/$', views.edit_story),
+    url(r'^publish/story/(?P<id>\d+)/$', views.publish_story),
+    url(r'^unpublish/story/(?P<id>\d+)/$', views.unpublish_story),
+    url(r'^save/story/(?P<story_type>[-\w]+)/(?P<vanity_url>[-\w]+)/$', views.new_story),
+    url(r'^delete/story/(?P<id>\d+)/$', views.delete_story),
+    url(r'^vote/up/(?P<story_id>\d+)/$', views.up_vote_story),
+    url(r'^vote/down/(?P<story_id>\d+)/$', views.down_vote_story),
+    url(r'^vote/none/(?P<story_id>\d+)/$', views.no_vote_story),
 
-    url(r'^tags/(?P<app_label>[-\w]+)/(?P<model>[-\w]+)$', "historyofcg.views.search", name="djtokeninput_search"),
-    url(r'^get/pages/$', 'historyofcg.views.get_pages', name='get_pages'),
+    url(r'^tags/(?P<app_label>[-\w]+)/(?P<model>[-\w]+)/$', views.search, name="djtokeninput_search"),
+    url(r'^get/pages/$', views.get_pages, name='get_pages'),
 
-    url(r'^add/connection/(?P<connect_to>[-\w]+)/(?P<to_connect>[-\w]+)/$', 'historyofcg.views.add_connection', name='add_connection'),
-    url(r'^remove/connection/(?P<remove_to>[-\w]+)/(?P<to_remove>[-\w]+)/$', 'historyofcg.views.remove_connection', name='remove_connection'),
+    url(r'^add/connection/(?P<connect_to>[-\w]+)/(?P<to_connect>[-\w]+)/$', views.add_connection, name='add_connection'),
+    url(r'^remove/connection/(?P<remove_to>[-\w]+)/(?P<to_remove>[-\w]+)/$', views.remove_connection, name='remove_connection'),
 
-    url(r'^user/(?P<i>\d+)/$', 'historyofcg.views.user_page')
-)
+    url(r'^user/(?P<i>\d+)/$', views.user_page)
+]
 
 urlpatterns += staticfiles_urlpatterns()
--- history/registration/backends/default/urls.py
+++ history/registration/backends/default/urls.py
@@ -18,34 +18,34 @@ up your own URL patterns for these views instead.
 """
 
 
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.views.generic import TemplateView
 
 from history.registration.views import activate
 from history.registration.views import register
 
 
-urlpatterns = patterns('',
-                       url(r'^activate/complete/$',
-                           TemplateView.as_view(template_name='registration/activation_complete.html'),
-                           name='registration_activation_complete'),
-                       # Activation keys get matched by \w+ instead of the more specific
-                       # [a-fA-F0-9]{40} because a bad activation key should still get to the view;
-                       # that way it can return a sensible "invalid key" message instead of a
-                       # confusing 404.
-                       url(r'^activate/(?P<activation_key>\w+)/$',
-                           activate,
-                           {'backend': 'history.registration.backends.default.DefaultBackend'},
-                           name='registration_activate'),
-                       url(r'^register/$',
-                           register,
-                           {'backend': 'history.registration.backends.default.DefaultBackend'},
-                           name='registration_register'),
-                       url(r'^register/complete/$',
-                           TemplateView.as_view(template_name='registration/registration_complete.html'),
-                           name='registration_complete'),
-                       url(r'^register/closed/$',
-                           TemplateView.as_view(template_name='registration/registration_closed.html'),
-                           name='registration_disallowed'),
-                       (r'', include('history.registration.auth_urls')),
-                       )
+urlpatterns = [
+    url(r'^activate/complete/$',
+        TemplateView.as_view(template_name='registration/activation_complete.html'),
+        name='registration_activation_complete'),
+    # Activation keys get matched by \w+ instead of the more specific
+    # [a-fA-F0-9]{40} because a bad activation key should still get to the view;
+    # that way it can return a sensible "invalid key" message instead of a
+    # confusing 404.
+    url(r'^activate/(?P<activation_key>\w+)/$',
+        activate,
+        {'backend': 'history.registration.backends.default.DefaultBackend'},
+        name='registration_activate'),
+    url(r'^register/$',
+        register,
+        {'backend': 'history.registration.backends.default.DefaultBackend'},
+        name='registration_register'),
+    url(r'^register/complete/$',
+        TemplateView.as_view(template_name='registration/registration_complete.html'),
+        name='registration_complete'),
+    url(r'^register/closed/$',
+        TemplateView.as_view(template_name='registration/registration_closed.html'),
+        name='registration_disallowed'),
+    url(r'', include('history.registration.auth_urls')),
+]
